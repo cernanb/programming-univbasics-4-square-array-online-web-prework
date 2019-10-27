25bf9e39e915ee90ba2168fd7f87cca04b4387a9
cernanb <cernanb@gmail.com>
2019-10-27 08:53:00 -0700

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  count = 0
+  arr = []
+# using while loop  
+# here condtional is x i.e. 4 
+  while count < array.length 
+    
+    # statements to be executed 
+    arr.push(array[count] * array[count])
+    count += 1 
+  # while loop ends here 
+  end
+  arr
 end
